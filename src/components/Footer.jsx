6c244e080f77eef531cdd16e6f5fde10e4812343
dm1tro69 <dimolg22@gmail.com>
2021-01-11 21:31:59 +0200

    add links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
             <div className="footer-copyright">
                 <div className="container">
                     © {new Date().getFullYear()} Copyright Text
-                    <a className="grey-text text-lighten-4 right" href="#">Repo</a>
+                    <a className="grey-text text-lighten-4 right" href="https://github.com/dm1tro69/React_shop">Repo</a>
                 </div>
             </div>
         </footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ function Header() {
     return (
         <nav className={'green darken-1'}>
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">React Shop</a>
+                <a href="/" className="brand-logo">React Shop</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="#">Repo</a></li>
+                    <li><a href="https://github.com/dm1tro69/React_shop/">Repo</a></li>
 
                 </ul>
             </div>
